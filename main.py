bee6cb17e79246262ad3515f61976cced9d7eaad
Mobas <gadiner1@gmail.com>
2023-11-13 15:23:58 +0300

    Create basic.Table

--- main.py
+++ main.py
@@ -12,14 +12,43 @@ for file in filepaths:
     pdf = FPDF(orientation='P', unit='mm', format='A4')
     pdf.set_auto_page_break(auto=False, margin=0)
     pdf.add_page()
-
+# Header
     filename = Path(file).stem
     invoice_nr = filename.split('-')[0]
     invoice_date = filename.split('-')[1]
     pdf.set_font(family='Times', style='B', size=12)
     pdf.set_text_color(0, 0, 0)
     pdf.cell(w=0, h=12, txt=f"Invoice nr. {invoice_nr}", align='L', ln=1)
-    pdf.cell(w=0, h=5, txt=f"Date {invoice_date}", align='L', ln=1)
+    pdf.cell(w=0, h=5, txt=f"Date {invoice_date}", align='L', ln=2)
+# Table Header
+    header_columns = df.columns
+    header_columns = [item.replace('_', ' ').title() for item in header_columns]
+    pdf.set_font(family='Times', size=10, style='B')
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt=f"{header_columns[0]}",
+             align='L', border=1)
+    pdf.cell(w=70, h=8, txt=f"{header_columns[1]}",
+             align='L', border=1)
+    pdf.cell(w=30, h=8, txt=f"{header_columns[2]}",
+             align='L', border=1)
+    pdf.cell(w=30, h=8, txt=f"{header_columns[3]}",
+             align='L', border=1)
+    pdf.cell(w=30, h=8, txt=f"{header_columns[4]}",
+             align='L', border=1, ln=1)
+# Table Rows
+    for i, row in df.iterrows():
+        pdf.set_font(family='Times', size=10)
+        pdf.set_text_color(80, 80, 80)
+        pdf.cell(w=30, h=8, txt=f"{row['product_id']}",
+                 align='L', border=1)
+        pdf.cell(w=70, h=8, txt=f"{row['product_name']}",
+                 align='L', border=1)
+        pdf.cell(w=30, h=8, txt=f"{row['amount_purchased']}",
+                 align='L', border=1)
+        pdf.cell(w=30, h=8, txt=f"{row['price_per_unit']}",
+                 align='L', border=1)
+        pdf.cell(w=30, h=8, txt=f"{row['total_price']}",
+                 align='L', border=1, ln=1)
 
     pdf.output(f'PDF/{filename}.pdf')
 
