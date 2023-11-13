d2dcfa66c015d19a801c06afda47a947a74ae458
Mobas <gadiner1@gmail.com>
2023-11-13 06:48:11 +0300

    Initial commit. Load excel into DF

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 from fpdf import FPDF
 import pandas as pd
+import glob
+import openpyxl
 
+# Input DATA
+filepaths = glob.glob('invoices/*.xlsx')
+
+for file in filepaths:
+    df = pd.read_excel(file, sheet_name='Sheet 1')
+
+    for i, row in df.iterrows():
+      print(f'File{i}\n'
+            f'{row}\n')
+
+
+
+
+
+
+# Output
